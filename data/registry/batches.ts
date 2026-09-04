a0e1d377a76e3a6f4c19230a5d9bb1f257c00328
Jenisch <kaan.atam.2@gmail.com>
2026-09-05 01:36:27 +0300

    refactor: register complete A seed batches

--- data/registry/batches.ts
+++ data/registry/batches.ts
@@ -1,4 +1,6 @@
 import seedBatchA01 from "./batches/trwiki-a-01.json";
 import seedBatchA02 from "./batches/trwiki-a-02.json";
+import seedBatchA03 from "./batches/trwiki-a-03.json";
+import seedBatchA04 from "./batches/trwiki-a-04.json";
 
-export const registrySeedBatches = [seedBatchA01, seedBatchA02] as const;
+export const registrySeedBatches = [seedBatchA01, seedBatchA02, seedBatchA03, seedBatchA04] as const;

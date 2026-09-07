560b73b50be6c665735c40bb5f63037482fbe70a
Jenisch <kaan.atam.2@gmail.com>
2026-09-07 18:54:12 +0300

    refactor: compose archive map batches

---
+++ data/registry/map-batches.ts
@@ -0,0 +1,7 @@
+import registryMapSitesBase from "./map-sites.json";
+import registryMapSitesE01 from "./map-batches/e-01.json";
+
+export const registryMapSitesData = [
+  ...registryMapSitesBase,
+  ...registryMapSitesE01,
+];

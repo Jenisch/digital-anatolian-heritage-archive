a95eabedfae5c8bbdc73c2197e633dacd9b629b0
Jenisch <kaan.atam.2@gmail.com>
2026-09-07 18:56:37 +0300

    chore: remove temporary map batch manifest

--- data/registry/map-batches.ts
+++
@@ -1,7 +0,0 @@
-import registryMapSitesBase from "./map-sites.json";
-import registryMapSitesE01 from "./map-batches/e-01.json";
-
-export const registryMapSitesData = [
-  ...registryMapSitesBase,
-  ...registryMapSitesE01,
-];
